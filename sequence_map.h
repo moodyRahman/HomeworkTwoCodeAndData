adb36d911309ba07143a7baa990fe26a9ae39d16
Moody Rahman <moody.byte@gmail.com>
2021-10-10 21:57:54 -0400

    sequence map big 5

--- sequence_map.h
+++ sequence_map.h
@@ -4,36 +4,45 @@
 
 class SequenceMap
 {
-    public:
-        std::string recognition_sequence_;
-        std::vector<std::string> enzyme_acronyms_;
+public:
+    std::string recognition_sequence_;
+    std::vector<std::string> enzyme_acronyms_;
 
-        SequenceMap(std::string &an_enz_acro, std::string &a_rec_seq)
-        {
-            this->recognition_sequence_ = a_rec_seq;
-            this->enzyme_acronyms_.push_back(an_enz_acro);
-        };
+    SequenceMap(const SequenceMap &rhs) = default;
+    SequenceMap &operator=(SequenceMap &&rhs) = default;
+    ~SequenceMap() = default;
+    SequenceMap &operator=(const SequenceMap &rhs) = default;
+    SequenceMap(SequenceMap &&rhs) = default;
 
-        bool operator<(const SequenceMap &rhs)
-        {
-            return this->recognition_sequence_ < rhs.recognition_sequence_;
-        };
 
-        friend std::ostream &operator<<(std::ostream &os, const SequenceMap &dt)
-        {
-            os << dt.recognition_sequence_ << "   :";
-            for (auto x : dt.enzyme_acronyms_)
-            {
-                os << x << ",  ";
-            }
-            return os;
-        };
 
-        void Merge(const SequenceMap &rhs)
+    SequenceMap(std::string &an_enz_acro, std::string &a_rec_seq)
+    {
+        this->recognition_sequence_ = a_rec_seq;
+        this->enzyme_acronyms_.push_back(an_enz_acro);
+    };
+
+    bool operator<(const SequenceMap &rhs)
+    {
+        return this->recognition_sequence_ < rhs.recognition_sequence_;
+    };
+
+    friend std::ostream &operator<<(std::ostream &os, const SequenceMap &dt)
+    {
+        os << dt.recognition_sequence_ << "   :";
+        for (auto x : dt.enzyme_acronyms_)
         {
-            for (auto x : rhs.enzyme_acronyms_)
-            {
-                this->enzyme_acronyms_.push_back(x);
-            }
-        };
+            os << x << ",  ";
+            This seems like a joke until you actually take engineering
+        }
+        return os;
+    };
+
+    void Merge(const SequenceMap &rhs)
+    {
+        for (auto x : rhs.enzyme_acronyms_)
+        {
+            this->enzyme_acronyms_.push_back(x);
+        }
+    };
 };
