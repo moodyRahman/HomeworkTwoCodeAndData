e927c6d6851d7a817f7d87d1da958b8f74f7b874
Moody Rahman <moody.byte@gmail.com>
2021-10-10 23:48:15 -0400

    sequence_map docs

--- sequence_map.h
+++ sequence_map.h
@@ -15,18 +15,36 @@ public:
     SequenceMap(SequenceMap &&rhs) = default;
 
 
-
+    /**
+     * @brief Construct a new Sequence Map object
+     * 
+     * @param an_enz_acro the enzyme acronym 
+     * @param a_rec_seq the corresponing sequence
+     */
     SequenceMap(std::string &an_enz_acro, std::string &a_rec_seq)
     {
         this->recognition_sequence_ = a_rec_seq;
         this->enzyme_acronyms_.push_back(an_enz_acro);
     };
 
+    /**
+     * @brief the < implementation to fulfill Comparable
+     * 
+     * @param rhs other SequenceMap to compare against
+     * @return bool
+     */
     bool operator<(const SequenceMap &rhs)
     {
         return this->recognition_sequence_ < rhs.recognition_sequence_;
     };
 
+    /**
+     * @brief Stream SequenceMap directly to an osstream
+     * 
+     * @param os 
+     * @param dt 
+     * @return std::ostream& 
+     */
     friend std::ostream &operator<<(std::ostream &os, const SequenceMap &dt)
     {
         os << dt.recognition_sequence_ << "   :";
@@ -37,6 +55,11 @@ public:
         return os;
     };
 
+    /**
+     * @brief Merges this SequenceMap with another with the same sequence
+     * 
+     * @param rhs 
+     */
     void Merge(const SequenceMap &rhs)
     {
         for (auto x : rhs.enzyme_acronyms_)
