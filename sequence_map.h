2ce0be86a4837f0657f96f30602d34d4d066e80d
Moody Rahman <moody.byte@gmail.com>
2021-10-10 20:46:35 -0400

    sequence_map class

--- sequence_map.h
+++ sequence_map.h
@@ -2,37 +2,38 @@
 #include <iostream>
 #include <vector>
 
-struct SequenceMap
+class SequenceMap
 {
-    std::string recognition_sequence_;
-    std::vector<std::string> enzyme_acronyms_;
+    public:
+        std::string recognition_sequence_;
+        std::vector<std::string> enzyme_acronyms_;
 
-    SequenceMap(std::string &an_enz_acro, std::string &a_rec_seq)
-    {
-        this->recognition_sequence_ = a_rec_seq;
-        this->enzyme_acronyms_.push_back(an_enz_acro);
-    };
-
-    bool operator<(const SequenceMap &rhs)
-    {
-        return this->recognition_sequence_ < rhs.recognition_sequence_;
-    };
-
-    friend std::ostream &operator<<(std::ostream &os, const SequenceMap &dt)
-    {
-        os << dt.recognition_sequence_ << "   :";
-        for (auto x : dt.enzyme_acronyms_)
+        SequenceMap(std::string &an_enz_acro, std::string &a_rec_seq)
         {
-            os << x << ",  ";
-        }
-        return os;
-    };
+            this->recognition_sequence_ = a_rec_seq;
+            this->enzyme_acronyms_.push_back(an_enz_acro);
+        };
 
-    void Merge(const SequenceMap &rhs)
-    {
-        for (auto x : rhs.enzyme_acronyms_)
+        bool operator<(const SequenceMap &rhs)
         {
-            this->enzyme_acronyms_.push_back(x);
-        }
-    };
+            return this->recognition_sequence_ < rhs.recognition_sequence_;
+        };
+
+        friend std::ostream &operator<<(std::ostream &os, const SequenceMap &dt)
+        {
+            os << dt.recognition_sequence_ << "   :";
+            for (auto x : dt.enzyme_acronyms_)
+            {
+                os << x << ",  ";
+            }
+            return os;
+        };
+
+        void Merge(const SequenceMap &rhs)
+        {
+            for (auto x : rhs.enzyme_acronyms_)
+            {
+                this->enzyme_acronyms_.push_back(x);
+            }
+        };
 };
